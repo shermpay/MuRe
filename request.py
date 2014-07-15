ec50b4f8f3b0ab5de3f3a6728fd21cb5a72a22d5
Sherman Pay <shermanpay1991@gmail.com>
2014-07-15 09:46:19 -0700

    Added doc strings

--- request.py
+++ request.py
@@ -10,18 +10,19 @@ import urllib.error as url_error
 def get_request_url(root_url, port=80, service_url='/'):
     """
     request_url(root_url[, port=80, service_url='/']) -> string
+    Transforms the url into a url that points to a request, given the parameters.
     """
-
     request_str_format = "{root_url}:{port}{service_url}"
     request_str = request_str_format.format(root_url=root_url,
                                             port=port, 
                                             service_url=service_url)
     return request_str
 
-def make_request(url='', request_type='GET', param_keys=(), param_vals=()):
+def make_request(url, request_type='GET', params={}):
     """
-    make_request(url=''[, request_type='GET', params=list()]) -> Request object
-    Make a HTTP request to url and returns a Request object.
+    make_request(url[, request_type='GET', params={}]]) -> Request object
+    Make a HTTP request to url with param.keys()=params.values() as request parameters.
+    Returns a Request object.
     Request object is defined in python urllib.
     To obtain the response from the Request object, use urllib.urlopen(Request).
     To read the data from the response, use response.read()
@@ -33,7 +34,14 @@ def make_request(url='', request_type='GET', param_keys=(), param_vals=()):
 
     return url_request.Request(url)
     
-def make_mult_requests(default_url='', request_type='GET', params_table=()):
+def make_mult_requests(default_url, request_type='GET', params_table=[])
+    """
+    make_mult_requests(default_url[, request_type='GET', paramas_table=[]]) -> list of Request object
+    Make a HTTP request to url with parameters specified in params_table.
+    params_table should have the param_keys as a tuple/list in the 0th index.
+    And each individual row should map the values to the keys by column index.
+    Returns a list of Request objects.
+    """
     param_keys = params_table.pop(0)
     requests = []
     for param_vals in params_table:
@@ -42,8 +50,8 @@ def make_mult_requests(default_url='', request_type='GET', params_table=()):
 
 def get_response(request):
     """
-    get_request_data(request) -> string
-    Trys to obtain data from request.
+    get_response(request) -> string
+    Trys to obtain data from Request object.
     Outputs error messages to stderr if HTTP error was encountered
     """
     try:
@@ -55,20 +63,39 @@ def get_response(request):
         return response_data
         
 def get_mult_response(request_list):
+    """
+    get_mult_response(request_list) -> list of strings
+    Trys to obtain responses from list of Request objects.
+    Returns a list of Response objects.
+    Outputs error messages to stderr if HTTP error was encountered.
+    """
     response_list = []
     for request in request_list:
         response_list.append(request_data(request))
     return response_list
 
 def read_config(file_path):
+    """
+    read_config(file_path) -> dict
+    Reads config from file_path
+    Returns a dict that holds the configuration
+    """
     with open(file_path) as config_file:
         config_dict = eval(config_file.read())
         return config_dict
 
 def get_requesters(config):
+    """
+    get_requesters(config) -> set
+    Returns a set of requesters
+    """
     return config.keys()
 
 def request_config(config, requester):
+    """
+    request_config(config, requester) -> dict
+    Returns a particular configuration for a requester given the config_dict
+    """
     return config[requester]
 
 if __name__ == '__main__':
