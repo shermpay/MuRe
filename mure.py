9a2a74227e48016147d1da10a5a66d940e906f41
Sherman Pay <shermanpay1991@gmail.com>
2014-07-17 17:55:56 -0700

    Using more argparse

--- mure.py
+++ mure.py
@@ -13,17 +13,16 @@ if __name__ == '__main__':
     requesters = config.requesters(conf)
     for requester in requesters:
         request_conf = config.get_requester(conf, requester)
-        url = request_conf['url']
-        port = request_conf['port']
         service_root = request_conf['service_root']
         for service in request_conf['services']:
-            request_url = request.get_request_url(url, port=port, 
+            request_url = request.get_request_url(request_conf['url'],
+                                                  port=request_conf['port'], 
                                                   service_url="{}{}".format(service_root, service))
             
             requests = request.make_mult_requests(default_url=request_url, 
                                                   method=request_conf['services'][service]['method'],
                                                   params_table=request_conf['services'][service]['params'])
-            print("Requests", requests)
+            print(["{}:{}".format(r.get_method(),r.get_full_url()) for r in requests])
             for req in requests:
                 response = request.get_response(req)
                 print(response)
