b79518747f2c03335864e21e241c8a2a9cac207e
shermpay <shermanpay1991@gmail.com>
2014-07-20 15:09:49 -0700

    Added verbose output, allowed -p flag with NO args, redesigned output.

--- mure.py
+++ mure.py
@@ -16,21 +16,22 @@ def main():
                         help="output contents to OUTPUT",)
     parser.add_argument("-v", "--verbose", action="store_true",
                         help="More verbose output. Default: Off.")
-    parser.add_argument("-p", "--pretty", default=0, type=int,
+    parser.add_argument("-p", "--pretty", default=0, type=int, nargs='?', const=1,
                          help="Pretty printing")
     parser.add_argument("-q", "--quiet", action="store_true",
                          help="Quiet mode. Minimal output.")
     args = parser.parse_args()
     if not args.quiet:
         intro()
-    execute(args.config_file, args.output, args.pretty)
+    exec_args = {'stream': args.output, 'pretty': args.pretty, 'verbose': args.verbose}
+    execute(args.config_file, **exec_args)
     
 def intro():
     print("MuRe: Multiple Requesting tool {}".format(VERSION))
     # Non-zero padded dates, with 24-hour clock
     print("Datetime: {}.\n".format(datetime.now().strftime("%y-%m-%d %H:%M:%S")))
 
-def execute(config_file, stream, pretty):
+def execute(config_file, **kwargs):
     print("* Reading config:", config_file)
     print()
     conf = config.Config(config_file)
@@ -39,37 +40,44 @@ def execute(config_file, stream, pretty):
     for requester in requesters:
         request_conf = conf.get_requester(requester)
         service_root = request_conf[key.service_root]
-        for service in request_conf[key.services].items():
+        for service_path, service_conf in request_conf[key.services].items():
+            service_str = "{}{}".format(service_root, service_path)
             request_url = request.get_request_url(request_conf[key.url],
                                                   port=request_conf[key.port], 
-                                                  service_url="{}{}".format(service_root, service[0]))
+                                                  service_url=service_str)
             
             requests = request.make_mult_requests(default_url=request_url, 
-                                                  method=service[1][key.method],
-                                                  params_table=service[1][key.params])
-            if (stream is sys.stdout):
-                print_requests_data(requests, pretty=pretty)
-            else:
-                print_requests_data(requests, stream=open(stream, 'a'), pretty=pretty)
+                                                  method=service_conf[key.method],
+                                                  params_table=service_conf[key.params])
+            
+            if (kwargs['stream'] is not sys.stdout):
+                kwargs['stream'] = open(stream, 'a')
+            
+            print_requests_data(requests, **kwargs)
 
-def print_requests_data(requests, stream=sys.stdout, pretty=0, verbose=False):
+def print_requests_data(requests, **kwargs):
+    stream = kwargs['stream']
+    pretty = kwargs['pretty']
+    verbose = kwargs['verbose']
+
     if pretty:
         pp = pprint.PrettyPrinter(indent=pretty, stream=stream)
     for req in requests:
-        print("[{}] {}".format(req.get_method(),req.get_full_url()), file=stream)
+        print("[{}]\t{}".format(req.get_method(),req.get_full_url()), file=stream)
         # Might log HTTP error
         response = request.get_response(req)
+        if verbose:
+            print('\tURI Scheme:', req.type)
+            print('\tHost:', req.host)
+            print('\tHeaders: ', req.header_items())
+
         if req.data is not None:
             print(req.data, file=stream)
-        
-        if verbose:
-            print('URI Scheme:', req.type)
-            print('Host:', req.host)
-            if req.has_header():
-                print('Header:', req.get_header())
 
         if response is not None:
             data = json.loads(response.decode(encoding='UTF-8'))
+            print('\nResponse')
+            print('========\n')
             if pretty:
                 pp.pprint(data)
             else:
@@ -78,6 +86,7 @@ def print_requests_data(requests, stream=sys.stdout, pretty=0, verbose=False):
             print("Request failed...", file=stream)
 
         print(file=stream)
+        print('--- End of Response ---\n', file=stream)
     
     if stream is not sys.stdout:
         stream.close()
